db97c4a7f6a847fc429281bc3677e0c62da613f5
Chan Li <130313462+chanlidev@users.noreply.github.com>
2025-06-15 21:19:26 -0600

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -222,10 +222,11 @@ const Index = () => {
               </h1>
               {/* Description */}
               <p className="text-white text-sm sm:text-base lg:text-lg max-w-2xl mx-auto leading-relaxed mb-12 lg:mb-16 drop-shadow-lg opacity-0 animate-fadeInUp animate-delay-300">
-                Hey there! 👋 I’m a Calgary-based UX/UI Designer with 5 years of
-                e-commerce magic under my belt 🛒✨. These days, I’m sprinkling
-                that design flair on payment flows at Pekopay, making every tap
-                and click a delight! 🚀😁
+                Hey there! 👋 I’m a Calgary-based UX/UI Designer with an
+                e-commerce data analysis background. I leverage user insights to
+                craft payment flows at Pekopay that are not only seamless but
+                measurably more intuitive, making every tap and click a delight!
+                🚀😁
               </p>
 
               {/* Contact Button */}
