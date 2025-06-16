a5cb0876cd647711fc712194ba8b97247a209a2d
Chan Li <130313462+chanlidev@users.noreply.github.com>
2025-06-15 22:32:15 -0600

    change

--- src/components/FeaturedWork.tsx
+++ src/components/FeaturedWork.tsx
@@ -59,11 +59,19 @@ const FeaturedWork = () => {
                   src="/landing.svg"
                   alt="Landing Preview"
                   className="w-4/5 h-auto object-contain rounded hover:rotate-12"
+                  style={{
+                    imageRendering: "crisp-edges",
+                    shapeRendering: "crispEdges",
+                  }}
                 />
                 <img
                   src="/loginphone.svg"
                   alt="Login Preview"
                   className="w-1/5 h-auto object-contain rounded hover:-rotate-12"
+                  style={{
+                    imageRendering: "crisp-edges",
+                    shapeRendering: "crispEdges",
+                  }}
                 />
               </div>
 
